ec08266f521acc731b98925228c0d54b0b4bb7de
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-11-15 00:20:13 +0100

    add jQuery to project

--- blogcafe/js/main.js
+++ blogcafe/js/main.js
@@ -14,6 +14,7 @@ $(function(){
 
         // $('.nuestros-servicios div').hide();
         // $(enlace).show();
+        
         $('.nuestros-servicios div').fadeOut();
         $(enlace).fadeIn();
         $(this).addClass('activo');
