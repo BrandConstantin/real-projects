7b4b71eb5ec959d0f6fef7b3a83a35065ed300ca
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-11-15 13:29:20 +0100

    jquery animateNumber

--- GDLWEBCAMP-html5/js/main.js
+++ GDLWEBCAMP-html5/js/main.js
@@ -12,4 +12,11 @@ $(function(){
 
         return false;
     });
+
+
+    // Animación de jQuery animateNumber
+    $('.resumen-evento li:first p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1600);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1200);
 });
